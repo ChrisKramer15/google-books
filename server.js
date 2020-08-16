931f7f4f9bc097ac085cfa068735372d5858e31c
Chris Kramer <ChrisKramer15@outlook.com>
2020-08-16 18:01:21 -0500

    server initialized

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const routes = require("./routes");
+const PORT = process.env.PORT || 9000;
+const app = express();
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//Mongoose connection
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/savedbooks_db", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+// app.use(routes);
+
+app.listen(PORT, function () {
+  console.log(`Now listening on port: ${PORT}!`);
+});

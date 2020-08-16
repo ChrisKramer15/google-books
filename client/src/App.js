462258c3895e227fc159e6167b1c44fcc9100393
Chris Kramer <ChrisKramer15@outlook.com>
2020-08-16 15:01:14 -0500

    Search component added, search field and button now visible

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
+      <Search />
     </div>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Header = () => {
+function Header() {
   return (
     <header>
       <h1>Google Books Search</h1>
     </header>
   );
-};
+}
 
 export default Header;
